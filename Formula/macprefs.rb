5947b2544c290a7d72baa80477585926a6e8d085
Knut Leborg <knut@lepa.no>
2023-01-05 04:40:56 +0100

    Update macprefs.rb

--- Formula/macprefs.rb
+++ Formula/macprefs.rb
@@ -4,7 +4,7 @@ class Macprefs < Formula
     desc "Backup and Restore your Mac System and App Preferences"
     homepage "https://github.com/knutle/macprefs"
     url "https://github.com/knutle/macprefs/archive/v1.1.0.tar.gz"
-    sha256 "03fcd5bd778d53fa9b89ec960b2fab18e5a8303ab15cbd79125def4cc9af8228"
+    sha256 "c217a43d0e353cb50d2a85de50656ab589e8abebb412b007849a39642e09340d"
   
     depends_on :python if MacOS.version <= :snow_leopard
   
